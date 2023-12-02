292d85e63d071126c24021802bd99a7b91c4e4ab
Russ Pedersen <pedersen@ds.cecs.pdx.edu>
2023-12-02 13:13:52 -0800

    Adding new python stuff

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPER_SECRET_PASSWORD = os.getenv("SUPER_SECRET_PASSWORD")
+print(f"THIS IS MY PASSWORD {SUPER_SECRET_PASSWORD}")
